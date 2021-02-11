9984585e998b08f382ada7320343843ab7e00f23
FOvchinnikov <approximation.sample@gmail.com>
2021-02-11 16:37:00 +0700

    Передача аргументов метода в виде массива

--- func_caller_serializer.cpp
+++ func_caller_serializer.cpp
@@ -5,13 +5,19 @@ namespace rpc::json_serialization
 
 void serialize_caller(const func_caller_base *fc, nlohmann::json &a, const type_map_t &types)
 {
-	a["name"] = fc->name;
-	a["return"] = get_type(fc->return_type, types);
-	auto &params = a["params"];
-	for(const auto &p : fc->params)
-	{
-		params[p.second] = get_type(p.first, types);
-	}
+    a["name"] = fc->name;
+    a["return"] = get_type(fc->return_type, types);
+
+    auto params = nlohmann::json::array();
+    nlohmann::json parameter_json;
+    for(const auto &p : fc->params)
+    {
+        parameter_json["name"] = p.second;
+        parameter_json["type"] = get_type(p.first, types);
+        params.push_back(parameter_json);
+    }
+
+    a["arguments"] = params;
 }
 
 }
